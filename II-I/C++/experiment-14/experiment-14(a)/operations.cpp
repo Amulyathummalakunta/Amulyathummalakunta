466ffdb7f480ae06c1aebc5854d35b393e8756d8
Amulyathummalakunta <97283171+Amulyathummalakunta@users.noreply.github.com>
2022-02-08 18:27:46 +0530

    the concept of console i/o operations

---
+++ II-I/C++/experiment-14/experiment-14(a)/operations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char x;
+cout<<"Enter the character:";
+cin.get(x);
+cout<<"The given Characteris:";
+cout.put(x);
+cout.write("\nWelcome to BVRIT",20);
+}
+
