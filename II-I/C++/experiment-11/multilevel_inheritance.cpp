7eee648990db307846210846bebc95c7024f04fd
Amulyathummalakunta <97283171+Amulyathummalakunta@users.noreply.github.com>
2022-02-08 18:06:52 +0530

    multilevel_inheritance

---
+++ II-I/C++/experiment-11/multilevel_inheritance.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class A1{
+public:
+int a;
+A1(){
+cin>>a;
+}
+void display1(){
+cout<<"a is "<<a<<endl;
+}
+};
+class A2:public A1{
+public:
+int b;
+A2(){
+cin>>b;
+}
+void display2(){
+cout<<"b is "<<b<<endl;
+}
+};
+class A3:public A2{
+public:
+int c;
+A3(){
+cin>>c;
+}
+void display3(){
+cout<<"c is "<<c<<endl;
+}
+};
+int main(){
+        A3 obj;
+        obj.display1();
+        obj.display2();
+        obj.display3();
+       return 0;
+}
